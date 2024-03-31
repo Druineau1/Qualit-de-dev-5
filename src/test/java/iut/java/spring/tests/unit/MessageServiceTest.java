84a32ae25ff150c0d9d1d486fdd0f6ab4a835991
Thomas Druineau <thomas.druineau@etu.unilim.fr>
2024-03-31 21:45:32 +0200

    ajout testSayHelloDefault

---
+++ src/test/java/iut/java/spring/tests/unit/MessageServiceTest.java
@@ -0,0 +1,26 @@
+package iut.java.spring.tests.unit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import iut.java.spring.dto.MessageDto;
+import iut.java.spring.service.interfaces.IMessageService;
+
+@SpringBootTest
+public class MessageServiceTest {
+
+    @Autowired
+    private IMessageService messageService;
+
+    @Test
+    public void testSayHelloDefault() {
+        //Appel de la méthode sayHello avec un paramètre null pour tester le comportement par défaut.
+        MessageDto message = messageService.sayHello(null);
+
+        //Vérification que le texte du message retourné est correct.
+        assertThat(message.getText()).isEqualTo("Hello World !");
+}
+}
